ceb07e23a6d77391e403d999c9e5c809f33e9957
Mohammed Habib <mhabib1981@gmail.com>
2015-04-06 04:50:04 -0700

    update

--- http_request_ext.py
+++ http_request_ext.py
@@ -1,75 +1,48 @@
 import sys
 import re
 
-if len(sys.argv)<3:
-        print ("Usage:\nhttp_request_ext.py file [OPTION]\n\n"+
-	      "AVAILABLE OPTIONS:\n"+ 
-	      "	-p\tCollect HTTP POST Requests\n"+
-	      "	-g\tCollect HTTP GET Requests\n"+
-	      "	-c\tCollect Cookie Values\n"+
-	      "	+h\tInclude HOST Address\n")
-	
-        sys.exit(0)
+def get_requests(line):
+	xr_gurl=re.sub(r'^(.*)GET',"",line).strip(' ')
+	host_info=get_host(line)
+        return (re.sub(r'HTTP/1.1(.*)',"",xr_gurl).strip('\r\n'))
 
-InFile=file(sys.argv[1], 'r').readlines()
+def post_requests(line):
+	xr_purl=re.sub(r'^(.*)POST',"",line)
+       	xl_purl=re.sub(r'HTTP/1.1(.*)',"",xr_purl).strip('\r\n')
+        return xl_purl.strip(' ')
 
-def get_requests(self):
-	for line in self:
-		if "GET" in line:
-                	xr_gurl=re.sub(r'^(.*)GET',"",line).strip(' ')
-                	xl_gurl=re.sub(r'HTTP/1.1(.*)',"",xr_gurl).strip('\r\n')
-        	        print xl_gurl
+def cookie(line):
+	xr_cookie=re.sub(r'^(.*)ie:',"",line)
+	print xr_cookie
 
-def post_requests(self):
-	for line in self:
-		if "POST" in line:
-			xr_purl=re.sub(r'^(.*)POST',"",line)
-        	        xl_purl=re.sub(r'HTTP/1.1(.*)',"",xr_purl).strip('\r\n')
-                	print xl_purl.strip(' ')
+def get_host(line):
+	return (re.sub(r'^(.*)st:',"",line)).strip('\r\n')
 
-def cookies(self):
-	for line in self:
-		if "Cookie" in line:
-			xr_cookie=re.sub(r'^(.*)ie:',"",line)
-			print xr_cookie
+def line_ext(file,opt):
+		host_info=''
+		for line in file:
+			if 'Host' in line:
+				host_info=get_host(line)
+			elif 'GET' in line and opt=='-g':
+				print (host_info + get_requests(line)).strip(' ')
+			elif 'POST' in line and opt=='-g':
+				print (host_info + post_requests(line)).strip(' ')
+			elif 'User-Agent' in line and opt == '-u':
+				print user-agent(line)
+			elif 'Cookie' in line and opt=='-c':
+				print cookie(line)
 
-def get_host(self):
-	for line in self:
-		if "Host" in line:
-			xr_host=re.sub(r'^(.*)st:',"",line)
-                	#xl_host=re.sub(r'(.*)',"",xr_host).strip('\r\n')
-			print line
-
-def get_uni_host_ip(self):
-		for line in self:
-			ip_pattn=re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-			ip_addr=re.findall(ip_pattn, line)
-			ip_addr_uni=[]
-			print ip_addr.index
-
-			#if ip_addr.index > 0 and ip_addr.index != ip_addr_uni.index -1:
-			#	ip_addr_uni.append(ip_addr)
-			#	print ip_addr_uni
-	
-		
-if sys.argv[2]=="-p":
-	post_requests(InFile)
-	sys.exit(0)
-
-elif sys.argv[2]=="-g":
-	get_requests(InFile)
-	sys.exit(0)
-
-elif sys.argv[2]=="-c":
-        cookies(InFile)
-        sys.exit(0)
-
-'''
-elif sys.argv[3]=="+h":
-	get_host(InFile)
-	sys.exit(0)
-'''
-
-if sys.argv[2]=="+ip":
-	get_uni_host_ip(InFile)
-	sys.exit(0)
+def main():
+	if len(sys.argv) < 3:
+		print ("Usage:\nhttp_request_ext.py file [OPTION]\n\n"+
+              		"AVAILABLE OPTIONS:\n"+
+              		" -p\tExtract HTTP POST\n"+
+              		" -g\tExtract HTTP GET\n"+
+             		" -c\tExtract Cookie Values\n")
+	        sys.exit(0)
+	else:
+		inputFile=file(sys.argv[1], 'r').readlines()
+		line_ext(inputFile, sys.argv[2])
+			
+if __name__ == '__main__':
+	main()
