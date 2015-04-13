7c7dadbfc5774cd5c62792249b8b9e4647db3fcb
Mohammed Habib <mhabib1981@gmail.com>
2015-04-13 06:35:16 -0700

    updated with unhex function

--- url_decode.py
+++ url_decode.py
@@ -1,16 +1,36 @@
 #!/usr/bin
 
 import sys
-import urllib
-#import base64
+from urllib import unquote
+import re
+from binascii import unhexlify
 
-if len(sys.argv)!=2:
-	print "Usage: url_decode.py file"
-	sys.exit(0)
+def main():
 
-inFile=file(sys.argv[1], 'r').readlines()
+	if len(sys.argv)!=2:
+		print "Usage: %s <file>" % sys.argv[0]
+		sys.exit(0)
+	else:
+		inFile=open(sys.argv[1], 'r').readlines()
+		for line in inFile:
+			decoded_url=url_decode(line)
+			if '0x' in decoded_url:
+				hex_val=re.findall(r'0x(.*?),',decoded_url)
+				print re.sub(r'0x(.*?),',hex_decode(hex_val[0]),decoded_url)
+			else:
+				print decoded_url
 
-for line in inFile:
-	url=urllib.unquote(line)
-	url_decode=url.decode('utf-8', 'ignore')
-	print url_decode
+def url_decode(input):
+	url=unquote(input)
+	return url.decode('utf-8', 'ignore')
+
+def hex_decode(input):
+	if len(input) % 2 ==0:
+		try:
+                 uhex_val=unhexlify(input)
+                 return uhex_val + ","
+                except TypeError:
+		 pass
+
+if __name__ == '__main__':
+	main()
